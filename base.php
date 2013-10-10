01e5082f37e6b9da96f3dbf51df94b696fa1e0db
Scott.P <footprintstartup@gmail.com>
2013-10-09 18:38:47 -0600

    update

---
+++ base.php
@@ -0,0 +1,29 @@
+<?php 
+// Include the page class
+include "page.php";
+// Use pull the link name and generate page object
+// Think about the code
+$page = new page;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>
+<?php 
+// Pass page object by reference
+printTitle($page);
+?>
+</title>
+
+</head>
+
+<body>
+<?php
+// Figure out how this will be dynamic
+?>
+</body>
+
+</html>
+
---
+++ page.php
@@ -0,0 +1,23 @@
+<?php
+// Definition of the page class
+
+
+
+class page
+{
+   // Default constructor
+   public function __construct()
+   {
+
+   }
+
+   //
+   //
+}
+
+
+
+
+
+
+?>
